5b0e1ffedda49b269b6ea19903531236012e8c24
Samuel Lima <dev.samuellc@outlook.com>
2025-06-23 00:09:01 -0300

    chore: updated websocket connect

--- lambda/websocket-service/src/handlers/connect.ts
+++ lambda/websocket-service/src/handlers/connect.ts
@@ -3,19 +3,29 @@ import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export const handler = async (event: APIGatewayEvent) => {
+export const handler = async (event: any) => {
   const connectionId = event.requestContext.connectionId;
+  
+  if (!connectionId) {
+    return { statusCode: 400, body: "Connection ID not found." };
+  }
 
-  if(!connectionId) throw new Error('Connection ID not found.')
+  try {
+    await prisma.webSocketConnection.create({
+      data: {
+        connectionId,
+      },
+    });
 
-  await prisma.webSocketConnection.create({
-    data: {
-      connectionId,
-    },
-  });
-
-  return {
-    statusCode: 200,
-    body: "Connected.",
-  };
+    return {
+      statusCode: 200,
+      body: "Connected.",
+    };
+  } catch (error) {
+    console.error("Erro ao salvar conexão:", error);
+    return {
+      statusCode: 500,
+      body: "Internal server error",
+    };
+  }
 };
